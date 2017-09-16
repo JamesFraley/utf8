30c0e2685d2a59dbf23ea307ee6fcf69d1c3cef4
James Fraley <JamesFraley@yahoo.com>
2017-09-16 12:54:07 -0400

    yup

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import "fmt"
-import "./printstuff"
+import "./printStuff"
 
 func main() {
+	var myString = `
+	<!DOCKTYPE html>
+	<html lang="en">
+	<body>
+	<meta charset="UTF-8"
+	Hello
+	</body>`
+
    fmt.Println("hello")
-   printstuff.MyPrint("bye")
+   printStuff.MyPrint(myString)
 }
--- printStuff/printStuff.go
+++ printStuff/printStuff.go
@@ -1,4 +1,4 @@
-package printstuff
+package printStuff
 
 import "fmt"
 
